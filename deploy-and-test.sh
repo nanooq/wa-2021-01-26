2c46800a0582186a002e06702ec7adefb666a7f1
nanoq <nanooq@nanooq.org>
2021-01-30 21:37:44 +0100

    Improve green audio tree area and bridge1 passing

--- deploy-and-test.sh
+++ deploy-and-test.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-pkill --signal 1 --full firefox
+pkill --signal 15 --full firefox
+trash ~/.mozilla/firefox/v2p3vu48.nanooq-wa/*
+trash ~/.cache/mozilla/firefox/v2p3vu48.nanooq-wa/*
+#firefox -purgecaches &
+#pkill --signal 15 --full firefox
 git status
 git commit --all --no-edit --amend
 git push -f
 sleep 2
-firefox -purgecaches --new-intance -P nanooq-wa --private-window https://world.chaosflux.de/_/global/nanooq.github.io/wa-2021-01-26/project/maps/main.json &
+firefox -purgecaches --new-instance -P nanooq-wa https://world.chaosflux.de/_/global/nanooq.github.io/wa-2021-01-26/project/maps/main.json &
 # https://play.workadventu.re/_/global/nanooq.github.io/wa-2021-01-26/project/maps/main.json
-xdotool search --sync --onlyvisible --class "Firefox" windowactivate key F11 &
+# xdotool search --sync --onlyvisible --class "Firefox" windowactivate key F11 &
